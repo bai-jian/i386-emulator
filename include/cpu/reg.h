8b16c209ff0626eebd8db40075674013f2f295fe
jyy <njujiangyy@gmail.com>
2014-09-28 19:48:39 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  19:48:39 up  2:44,  3 users,  load average: 0.09, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash---vi      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---vi      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- include/cpu/reg.h
+++ include/cpu/reg.h
@@ -11,23 +11,20 @@
  * For more details about the GPR encoding scheme, see i386 manual.
  */
 
-typedef struct {
-	union {
+typedef struct
+{
+	union
+   	{
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
 	} gpr[8];
 
-	/* Do NOT change the order of the GPRs' definitions. 
-	 * They match the register encoding scheme used in i386 instruction format.
-	 * See i386 manual for more details.
-	 */
-
 	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
-
-
 	swaddr_t eip;
+	uint32_t eflags;
+	uint16_t CS, SS, DS, ES, FS, GS;
 } CPU_state;
 
 extern CPU_state cpu;
