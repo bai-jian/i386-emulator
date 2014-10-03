bb79026df57a95cde2dc502577d037f6b2fb97f9
jyy <njujiangyy@gmail.com>
2014-10-03 15:24:08 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  15:24:08 up  6:09,  4 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      |-sshd---sshd---bash---gdb---a.out      |      `-sshd---sshd---bash      `-udevd---2*[udevd]

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -102,7 +102,8 @@ static void cmd_info()
 	char* p = strtok_r(NULL, " ", &saveptr);
 
 	if (strcmp(p, "r") == 0)
-		printf("eax = %8X\tecx = %8X\tedx = %8X\tebx = %8X\nebp = %8X\tesp = %8X\tesi = %8X\tedi = %8X\n",\
+		printf("eax = 0x%8X\t ecx = 0x%8X\t edx = 0x%8X\t ebx = 0x%8X\n\
+				ebp = 0x%8X\t esp = 0x%8X\t esi = 0x%8X\t edi = 0x%8X\n",\
 				cpu.eax, cpu.ecx, cpu.edx, cpu.ebx,\
 				cpu.ebp, cpu.esp, cpu.esi, cpu.edi);
 	else 
