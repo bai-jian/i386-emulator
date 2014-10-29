397fc524c281e1080367c4766db7dd23dc65a6db
jyy <njujiangyy@gmail.com>
2014-10-29 17:33:01 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  17:33:01 up  4:24,  3 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd---2*[sshd---sshd---bash---tmux]      |-tmux-+-2*[bash]      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      `-bash---vi      `-udevd---2*[udevd]

--- src/exec/CTI/jmp/jmp-template.h
+++ src/exec/CTI/jmp/jmp-template.h
@@ -5,7 +5,7 @@ make_helper( concat(jmp_i_, SUFFIX) )
 {
 	uint32_t imm = instr_fetch(eip+1, DATA_BYTE);
 
-	print_asm("jmp" " 0x%x", eip + 1+DATA_BYTE + imm);
+	print_asm("jmp" " 0x%x", eip + 1 + DATA_BYTE + imm);
 
 	return 1 + DATA_BYTE + imm;
 }
