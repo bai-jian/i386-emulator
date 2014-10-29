a63ec84a560e3d985cbb45c15c461a18e3786e95
jyy <njujiangyy@gmail.com>
2014-10-29 17:00:28 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  17:00:28 up  3:52,  3 users,  load average: 0.02, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash---make---sh-+-git      |                          `-pstree      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      `-sshd---sshd---bash      |-tmux---2*[bash]      `-udevd---2*[udevd]

--- include/exec/ALI/sub/sub.h
+++ include/exec/ALI/sub/sub.h
@@ -1,19 +1,3 @@
-#ifndef __ALI__SUB__
-#define __ALI__SUB__
-
-
 make_helper(sub_i2r0_b);
-make_helper(sub_i2r0_v);
-
-
-make_helper(sub_i2rm_b);
-make_helper(sub_i2rm_v);
-
-make_helper(sub_r2rm_b);
-make_helper(sub_r2rm_v);
-
-make_helper(sub_rm2r_b);
-make_helper(sub_rm2r_v);
-
-
-#endif
+make_helper(sub_i2r0_w);
+make_helper(sub_i2r0_l);
