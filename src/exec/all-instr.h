5c64a9978df5925a0e7409307ff33b45e43451b3
jyy <njujiangyy@gmail.com>
2014-10-31 23:03:45 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  23:03:45 up 1 day,  6:45,  3 users,  load average: 0.01, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      `-sshd---sshd---bash      |-tmux-+-bash      |      `-bash---make---sh-+-git      |                         `-pstree      `-udevd---2*[udevd]

--- src/exec/all-instr.h
+++ src/exec/all-instr.h
@@ -1,10 +1,7 @@
 #include "prefix/prefix.h"
+#include "misc/misc.h"
 
 
 /* ALI: arithmetic & logical instructions */  #include "ALI/ALI.h"
 /* CTI: control transfer instructions     */  #include "CTI/CTI.h"
 /* DMI: data movement instructions        */  #include "DMI/DMI.h"
-
-#include "data-mov/mov.h"
-
-#include "misc/misc.h"
