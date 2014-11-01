647d598d8d88f7b353945d5c96118de68dd6d47b
jyy <njujiangyy@gmail.com>
2014-11-01 11:08:12 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  11:08:12 up 1 day,  8:48,  3 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd---2*[sshd---sshd---bash---tmux]      |-tmux-+-2*[bash]      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      `-bash---vi      `-udevd---2*[udevd]

--- src/exec/ALI/ALI_3/ALI_3-opcode.c
+++ src/exec/ALI/ALI_3/ALI_3-opcode.c
@@ -4,6 +4,7 @@
 
 #include "add/add.h"
 #include "adc/adc.h"
+#include "sbb/sbb.h"
 #include "and/and.h"
 #include "sub/sub.h"
 #include "cmp/cmp.h"
@@ -13,6 +14,7 @@ make_helper(ALI_3_b)
 	ModR_M m;	m.val = instr_fetch(eip+1, 1);
 	if (m.reg == 0)	 return  add_i2rm_b(eip);
 	if (m.reg == 2)  return  adc_i2rm_b(eip);
+	if (m.reg == 3)  return  sbb_i2rm_b(eip);
 	if (m.reg == 4)  return  and_i2rm_b(eip);
 	if (m.reg == 5)  return  sub_i2rm_b(eip);
 	if (m.reg == 7)  return  cmp_i2rm_b(eip);
@@ -24,6 +26,7 @@ make_helper(ALI_3_v)
 	ModR_M m;	m.val = instr_fetch(eip+1, 1);
 	if (m.reg == 0)	 return  add_i2rm_v(eip);
 	if (m.reg == 2)  return  adc_i2rm_v(eip);
+	if (m.reg == 3)  return  sbb_i2rm_v(eip);
 	if (m.reg == 4)  return  and_i2rm_v(eip);
 	if (m.reg == 5)  return  sub_i2rm_v(eip);
 	if (m.reg == 7)  return  cmp_i2rm_v(eip);
