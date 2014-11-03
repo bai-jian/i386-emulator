47c06ebdf94b534997bdf21a52a4e192cb35b789
jyy <njujiangyy@gmail.com>
2014-11-03 17:24:09 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  17:24:09 up 2 days, 3 min,  3 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd---2*[sshd---sshd---bash---tmux]      |-tmux-+-3*[bash]      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      `-2*[bash---vi]      `-udevd---2*[udevd]

--- src/exec/ALI/ALI_1/call/call.c
+++ src/exec/ALI/ALI_1/call/call.c
@@ -11,6 +11,8 @@ make_helper(call_i)
 	cpu.esp -= 4;	swaddr_write(cpu.esp, 4, cpu.eip+5);
 	cpu.eip += (5 + imm);
 
+	print_asm("call" "0x%x", cpu.eip);
+
 	return 0;
 }
 
@@ -18,17 +20,19 @@ make_helper(call_rm)
 {
 	ModR_M m;	m.val = instr_fetch(eip+1, 1);
 
-	int32_t disp;
+	int32_t disp; uint8_t len;
 	if (m.mod != 3)
 	{
-		swaddr_t mem_i;	read_ModR_M(eip+1, &mem_i);
+		swaddr_t mem_i;	len = 1 + read_ModR_M(eip+1, &mem_i);
 		disp = swaddr_read(mem_i, 4);
 	}
 	else
-		disp = reg_l(m.R_M);
+		disp = reg_l(m.R_M), len = 2;
 
-	cpu.esp -= 4;	swaddr_write(cpu.esp, 4, cpu.eip);
-	cpu.eip += disp;
+	cpu.esp -= 4;	swaddr_write(cpu.esp, 4, cpu.eip+len);
+	cpu.eip += (len + disp);
+
+	print_asm("call" "0x%x", cpu.eip);
 
 	return 0;
 }
--- src/exec/CTI/CTI_2/ret/ret.c
+++ src/exec/CTI/CTI_2/ret/ret.c
@@ -8,5 +8,9 @@ make_helper(ret)
 	uint32_t disp = swaddr_read(cpu.esp, 4);	cpu.esp += 4;
 	cpu.eip = disp;
 
+	assert(cpu.eip == 0x10000b);
+
+	print_asm(" ");
+
 	return 0;
 }
