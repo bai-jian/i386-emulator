5e15eb08c48342def20b4fd774f194d96d0cf637
jyy <njujiangyy@gmail.com>
2014-11-08 18:23:30 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  18:23:30 up 57 min,  3 users,  load average: 0.09, 0.05, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd---2*[sshd---sshd---bash---tmux]      |-tmux-+-2*[bash]      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      `-bash---vi      `-udevd---2*[udevd]

--- src/exec/CTI/CTI_2/ret/ret.c
+++ src/exec/CTI/CTI_2/ret/ret.c
@@ -6,13 +6,8 @@
 make_helper(ret)
 {
 	uint8_t instr_len = 1;
-
 	uint32_t disp = swaddr_read(cpu.esp, 4);	cpu.esp += 4;
 	cpu.eip = disp - instr_len;
-
-	printf("0x%x/n", disp);
-
 	print_asm("ret");
-
 	return instr_len;
 }
