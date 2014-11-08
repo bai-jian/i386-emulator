ad3dca3f84336e14d28fd1b8daab48af38fcd610
jyy <njujiangyy@gmail.com>
2014-11-08 19:32:45 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  19:32:45 up  1:44,  3 users,  load average: 0.09, 0.05, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash---vi      |-rsyslogd---3*[{rsyslogd}]      |-sshd---2*[sshd---sshd---bash---tmux]      |-tmux-+-2*[bash]      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      `-bash---vi      `-udevd---2*[udevd]

--- src/exec/ALI/ALI_1/push/push-template.h
+++ src/exec/ALI/ALI_1/push/push-template.h
@@ -8,7 +8,7 @@ make_helper( concat(push_i_, SUFFIX) )
 	DATA_TYPE imm = instr_fetch(eip+1, DATA_BYTE);
 
 	cpu.esp -= DATA_BYTE;
-	MEM_W( cpu.esp, imm );
+	MEM_W(cpu.esp, imm);
 
 	print_asm("push" str(SUFFIX) " $0x%x", imm);
 
@@ -17,24 +17,24 @@ make_helper( concat(push_i_, SUFFIX) )
 
 make_helper( concat(push_r_, SUFFIX) )
 { 
-	uint8_t reg_code = instr_fetch(eip, 1) & 0x07;
+	uint8_t   reg_i = instr_fetch(eip, 1) & 0x07;
+	DATA_TYPE reg_v = REG(reg_i);
 
 	cpu.esp -= DATA_BYTE;
-	MEM_W( cpu.esp, REG(reg_code) );
+	MEM_W(cpu.esp, reg_v);
 	
-	print_asm("push" str(SUFFIX) " %%%s", REG_NAME(reg_code));
+	print_asm("push" str(SUFFIX) " %%%s", REG_NAME(reg_i));
 
 	return 1;
 }
 
 make_helper( concat(push_m_, SUFFIX) )
 {
-	swaddr_t addr;
-	uint32_t len = read_ModR_M(eip+1, &addr);
-	DATA_TYPE mem = MEM_R(addr);
+	swaddr_t  mem_i;  uint32_t len = read_ModR_M(eip+1, &mem_i);
+	DATA_TYPE mem_v = MEM_R(mem_i);
 
 	cpu.esp -= DATA_BYTE;
-	MEM_W(cpu.esp, mem);
+	MEM_W(cpu.esp, mem_v);
 
 	print_asm("push" str(SUFFIX) " %s", ModR_M_asm); 
 
