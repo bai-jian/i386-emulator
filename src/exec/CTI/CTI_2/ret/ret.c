6f411c3ee4fe82f5b1ccb6cad5d720e78140a733
jyy <njujiangyy@gmail.com>
2014-11-08 19:02:39 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  19:02:39 up  1:14,  3 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd---2*[sshd---sshd---bash---tmux]      |-tmux-+-2*[bash]      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      `-bash---vi      `-udevd---2*[udevd]

--- src/exec/CTI/CTI_2/ret/ret.c
+++ src/exec/CTI/CTI_2/ret/ret.c
@@ -8,6 +8,6 @@ make_helper(ret)
 	uint8_t instr_len = 1;
 	uint32_t disp = swaddr_read(cpu.esp, 4);	cpu.esp += 4;
 	cpu.eip = disp - instr_len;
-	print_asm("ret");
+	print_asm("ret   ");
 	return instr_len;
 }
--- src/exec/CTI/jcc/jcc-template.h
+++ src/exec/CTI/jcc/jcc-template.h
@@ -7,62 +7,62 @@ make_helper( concat(je_, SUFFIX) )
 	uint8_t instr_len = 1 + DATA_BYTE;
 	DATA_TYPE_S disp = instr_fetch(eip+1, DATA_BYTE);
 	cpu.eip += ( cpu.ZF ? disp : 0 );
-	print_asm("je" " %x", eip + instr_len + disp);
+	print_asm("je    " "%x", eip + instr_len + disp);
 	return instr_len;
 }
 	
 make_helper( concat(jne_, SUFFIX) )
 {
+	uint8_t instr_len = 1 + DATA_BYTE;
 	DATA_TYPE_S disp = instr_fetch(eip+1, DATA_BYTE);
-
-	print_asm("jne" " %x", eip + 1 + DATA_BYTE + disp);
-
-	return !cpu.ZF ? (1 + DATA_BYTE + disp) : (1 + DATA_BYTE);
+	cpu.eip += ( !cpu.ZF ? disp : 0 );
+	print_asm("jne   " "%x", eip + instr_len + disp);
+	return instr_len;
 }
 
 make_helper( concat(ja_, SUFFIX) )
 {
+	uint8_t instr_len = 1 + DATA_BYTE;
 	DATA_TYPE_S disp = instr_fetch(eip+1, DATA_BYTE);
-
-	print_asm("ja" " %x", eip + 1 + DATA_BYTE + disp);
-
-	return (!cpu.CF && !cpu.ZF) ? (1 + DATA_BYTE + disp) : (1 + DATA_BYTE);
+	cpu.eip += ((!cpu.CF && !cpu.ZF) ? disp : 0); 
+	print_asm("ja    " "%x", eip + instr_len + disp);
+	return instr_len;
 }
 
 make_helper( concat(jb_, SUFFIX) )
 {
+	uint8_t instr_len = 1 + DATA_BYTE;
 	DATA_TYPE_S disp = instr_fetch(eip+1, DATA_BYTE);
-
-	print_asm("jb" " %x", eip + 1 + DATA_BYTE + disp);
-
-	return cpu.CF ? (1 + DATA_BYTE + disp) : (1 + DATA_BYTE);
+	cpu.eip += (cpu.CF ? disp : 0);
+	print_asm("jb    " "%x", eip + instr_len + disp);
+	return instr_len;
 }
 
 make_helper( concat(jae_, SUFFIX) )
 {
+	uint8_t instr_len = 1 + DATA_BYTE;
 	DATA_TYPE_S disp = instr_fetch(eip+1, DATA_BYTE);
-
-	print_asm("jae" " %x", eip + 1 + DATA_BYTE + disp);
-
-	return !cpu.CF ? (1 + DATA_BYTE + disp) : (1 + DATA_BYTE);
+	cpu.eip += (!cpu.CF ? disp : 0);
+	print_asm("jae   " "%x", eip + instr_len + disp);
+	return instr_len;
 }
 
 make_helper( concat(jbe_, SUFFIX) )
 {
+	uint8_t instr_len = 1 + DATA_BYTE;
 	DATA_TYPE_S disp = instr_fetch(eip+1, DATA_BYTE);
-
-	print_asm("jbe" " %x", eip + 1 + DATA_BYTE + disp);
-
-	return (cpu.CF || cpu.ZF) ? (1 + DATA_BYTE + disp) : (1 + DATA_BYTE);
+	cpu.eip += ((cpu.CF || cpu.ZF) ? disp : 0);
+	print_asm("jbe   " "%x", eip + instr_len + disp);
+	return instr_len;
 }
 
 make_helper( concat(jg_, SUFFIX) )
 {
+	uint8_t instr_len = 1 + DATA_BYTE;
 	DATA_TYPE_S disp = instr_fetch(eip+1, DATA_BYTE);
-
-	print_asm("jg" " %x", eip + 1 + DATA_BYTE + disp);
-
-	return (!cpu.ZF && cpu.SF == cpu.OF) ? (1 + DATA_BYTE + disp) : (1 + DATA_BYTE); 
+	cpu.eip += ((!cpu.ZF && cpu.SF == cpu.OF) ? disp : 0);
+	print_asm("jg    " "%x", eip + instr_len + disp);
+	return instr_len;
 }
 
 make_helper( concat(jle_, SUFFIX) )
