94195bdd24a017ee14ffdc4a25c5e257afbf4ec7
jyy <njujiangyy@gmail.com>
2014-11-09 16:24:22 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  16:24:22 up 10:28,  4 users,  load average: 0.02, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash---vi      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      |-sshd---sshd---bash---vi      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      |-tmux---2*[bash]      `-udevd---2*[udevd]

--- src/exec/CTI/jcc/jcc.h
+++ src/exec/CTI/jcc/jcc.h
@@ -3,17 +3,47 @@
 
 
 make_helper(je_b);		make_helper(je_w);		make_helper(je_l);
+//make_helper(jz_b);	make_helper(jz_w);		make_helper(jz_l);
 make_helper(jne_b);		make_helper(jne_w);		make_helper(jne_l);
+//make_helper(jnz_b);	make_helper(jnz_w);		make_helper(jnz_l);
 
 make_helper(ja_b);		make_helper(ja_w);		make_helper(ja_l);
+//make_helper(jnbe_b);	make_helper(jnbe_w);	make_helper(jnbe_l);
 make_helper(jb_b);		make_helper(jb_w);		make_helper(jb_l);
+//make_helper(jnae_b);	make_helper(jnbe_w);	make_helper(jnbe_l);
 make_helper(jae_b);		make_helper(jae_w);		make_helper(jae_l);
+//make_helper(jnb_b);	make_helper(jnb_w);		make_helper(jnb_l);
 make_helper(jbe_b);		make_helper(jbe_w);		make_helper(jbe_l);
+//make_helper(jna_b);	make_helper(jnb_w);		make_helper(jnb_l);
 
 make_helper(jg_b);		make_helper(jg_w);		make_helper(jg_l);
+//make_helper(jnle_b);	make_helper(jnle_w);	make_helper(jnle_l);
 make_helper(jl_b);		make_helper(jl_w);		make_helper(jl_l);
+//make_helper(jnge_b);	make_helper(jnge_w);	make_helper(jnge_l);
 make_helper(jge_b);		make_helper(jge_w);		make_helper(jge_l);
+//make_helper(jnl_b);	make_helper(jnl_w);		make_helper(jnl_l);
 make_helper(jle_b);		make_helper(jle_w);		make_helper(jle_l);
+//make_helper(jng_b);	make_helper(jng_w);		make_helper(jng_l);
+/*
+make_helper(jc_b);		make_helper(jc_w);		make_helper(jc_l);
+make_helper(jnc_b);		make_helper(jnc_w);		make_helper(jnc_l);
 
+make_helper(jo_b);		make_helper(jo_w);		make_helper(jo_l);
+make_helper(jno_b);		make_helper(jno_w);		make_helper(jno_l);
+
+make_helper(jz_b);		make_helper(jz_w);		make_helper(jz_l);
+make_helper(jnz_b);		make_helper(jnz_w);		make_helper(jnz_l);
+
+make_helper(js_b);		make_helper(js_w);		make_helper(js_l);
+make_helper(js_b);		make_helper(js_w);		make_helper(js_l);
+
+make_helper(jp_b);		make_helper(jp_w);		make_helper(jp_l);
+make_helper(jnp_b);		make_helper(jnp_w);		make_helper(jnp_l);
+make_helper(jpe_b);		make_helper(jpe_w);		make_helper(jpe_l);
+make_helper(jpo_b);		make_helper(jpo_w);		make_helper(jpo_l);
+
+make_helper(jcxz_b);	make_helper(jcxz_w);	make_helper(jcxz_l);
+make_helper(jecxz_b);	maek_helper(jecxz_w);	make_helper(jecxz_l);
+*/
 
 #endif
