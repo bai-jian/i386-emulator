b5666d63cb555b5cca6f1cfa1103e6bdd8c977fc
jyy <njujiangyy@gmail.com>
2014-11-10 13:21:01 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  13:21:01 up 21:56,  4 users,  load average: 0.21, 0.24, 0.14 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash---vi      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      |-tmux---2*[bash]      `-udevd---2*[udevd]

--- testcase/c/if-else.c
+++ testcase/c/if-else.c
@@ -14,11 +14,13 @@ int if_else(int n) {
 int test_data[] = {-1, 0, 49, 50, 51, 99, 100, 101, 299, 300, 301, 499, 500, 501};
 int ans[] = {0, 0, 0, 0, 50, 50, 50, 75, 75, 75, 100, 100, 100, 150};
 
+#define N 14
+
 #define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
 	int i, ans_idx = 0;
-	for(i = 0; i < NR_DATA; i ++) {
+	for(i = 0; i < N; i ++) {
 		nemu_assert(if_else(test_data[i]) == ans[ans_idx ++]);
 	}
 
