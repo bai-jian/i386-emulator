57df334a67516e13ac7d45fa1e251f045575cae9
jyy <njujiangyy@gmail.com>
2014-11-11 08:56:04 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  08:56:04 up 1 day,  9:22,  4 users,  load average: 0.01, 0.04, 0.05 init-+-bash---tmux      |-bash---vi      |-bash-+-make---sh-+-git      |      |           `-pstree      |      `-3*[make---sh-+-nemu]      |                     `-tee]      |-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---tmux]      |      `-sshd---sshd---bash-+-make---sh-+-git      |                           |           `-pstree      |                           `-make---sh-+-nemu      |                                       `-tee      |-tmux-+-4*[bash---vi]      |      `-2*[bash]      `-udevd---2*[udevd]

---
+++ src/exec/DMI/DMI_1/movsx/movsx.c
@@ -0,0 +1,62 @@
+#include "exec/helper.h"
+
+#include "cpu/reg.h"
+#include "cpu/modrm.h"
+
+extern char suffix;
+
+make_helper(movsbv)
+{
+	if (suffix == 'w')
+	{
+		ModR_M m;  m.val = instr_fetch(eip+1, 1);
+		if (m.mod != 3)
+		{
+			swaddr_t mem_i;  uint8_t len = read_ModR_M(eip+1, &mem_i);
+			swaddr_write(mem_i, 2, (int8_t)reg_b(m.reg));
+			print_asm("movsbw  " "%%%s,%s", regsb[m.reg], ModR_M_asm);
+			return 1 + len;
+		}
+		else
+		{
+			reg_w(m.R_M) = (int8_t)reg_b(m.reg);
+			print_asm("movsbw  " "%%%s,%%%s", regsb[m.reg], regsw[m.R_M]);
+			return 1 + 1;
+		}
+	}
+	else
+	{
+		ModR_M m;  m.val = instr_fetch(eip+1, 1);
+		if (m.mod != 3)
+		{
+			swaddr_t mem_i;  uint8_t len = read_ModR_M(eip+1, &mem_i);
+			swaddr_write(mem_i, 4, (int8_t)reg_b(m.reg));
+			print_asm("movsbl  " "%%%s,%s", regsb[m.reg], ModR_M_asm);
+			return 1 + len;
+		}
+		else
+		{
+			reg_l(m.R_M) = (int8_t)reg_b(m.reg);
+			print_asm("movsbl  " "%%%s,%%%s", regsb[m.reg], regsl[m.R_M]);
+			return 1 + 1;
+		}
+	}
+}
+
+make_helper(movswl)
+{
+	ModR_M m;  m.val = instr_fetch(eip+1, 1);
+	if (m.mod != 3)
+	{
+		swaddr_t mem_i;  uint8_t len = read_ModR_M(eip+1, &mem_i);
+		swaddr_write(mem_i, 4, (int16_t)reg_w(m.reg));
+		print_asm("movswl  " "%%%s,%s", regsw[m.reg], ModR_M_asm);
+		return 1 + len;
+	}
+	else
+	{
+		reg_l(m.R_M) = (int16_t)reg_w(m.reg);
+		print_asm("movswl  " "%%%s,%%%s", regsw[m.reg], regsl[m.R_M]);
+		return 1 + 1;
+	}
+}
---
+++ src/exec/DMI/DMI_1/movsx/movsx.h
@@ -0,0 +1,11 @@
+#ifndef __DMI_MOVSX__
+#define __DMI_MOVSX__
+
+
+make_helper(movsbv);
+//make_helper(movsbw);
+//make_helper(movsbl);
+make_helper(movswl);
+
+
+#endif
