75917b2ff683fda3ad8344edc47c7073d8a3b37d
jyy <njujiangyy@gmail.com>
2014-12-08 23:07:25 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  23:07:25 up 1 day,  3:11,  3 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd---2*[sshd---sshd---bash---tmux]      |-tmux-+-6*[bash]      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      `-bash---vi      `-udevd---2*[udevd]

--- src/memory/cache.c
+++ src/memory/cache.c
@@ -37,11 +37,14 @@ typedef union
 	uint32_t addr;
 	struct
 	{
-		uint32_t byte	: AIB_WIDTH;
-		uint32_t index	: IDX_WIDTH;
+		uint32_t aib	: AIB_WIDTH;
+		uint32_t idx	: IDX_WIDTH;
 		uint32_t tag	: TAG_WIDTH;
 	};
 } cache_addr;
+
+#define CA2PA_BLK(tag, idx)  ((tag) << (AIB_WIDTH+IDX_WIDTH)) + ((idx) << AIB_WIDTH)
+#define BLK_ADDR(addr)  ((addr) >> AIB_WIDTH << AIB_WIDTH)
 
 
 /* the time counter of Cache hit and miss */
@@ -66,19 +69,61 @@ void init_cache( )
 
 uint32_t cache_read(hwaddr_t addr, size_t len)
 {
-/*	cache_addr fstb;  fstb.addr = addr;            //fstb: the first byte
+	cache_addr fstb;  fstb.addr = addr;            //fstb: the first byte
 	cache_addr lstb;  lstb.addr = addr + len - 1;  //lstb: the last byte
-	*/
+	
+	// Judge whether data crosses the boundary
+	if (fstb.idx != lstb.idx)  // data crossing
+	{
+		hwaddr_t addr2 = BLK_ADDR(lstb.addr);
+		size_t len1 = addr2 - addr;
+		size_t len2 = len - len1;
+		uint32_t data1 = cache_read(addr, len1);
+		uint32_t data2 = cache_read(addr2, len2);
+		return (data2 << (8*len1)) + data1;  // little endian
+	}
+	else                       // data not crossing
+	{
+		// Judge hit or miss
+		int way;
+		for (way = 0; way < NR_WAY; ++way)
+			if ( block[fstb.idx][way].valid && (block[fstb.idx][way].tag == fstb.tag) )
+				break;
 
-	uint8_t data[2 * NR_BIB];
+		// hit or miss calculates
+		(way < NR_WAY)  ?  ++hit  :  ++miss;
+
+		if (way == NR_WAY)  //miss
+		{
+			// Judge load or replace
+			// Replacement Algorithm: randomized algorithm, replace BLOCK 0
+			int i;
+			for (i = 0; i < NR_WAY; ++i)
+				if ( !block[fstb.idx][i].valid )
+					break;
+			way = (i < NR_WAY) ? i : 0;
+
+			block[fstb.idx][way].valid = 1;
+			block[fstb.idx][way].tag = fstb.tag;
+
+			hwaddr_t blk_addr = BLK_ADDR(fstb.addr);
+			for (i = 0; i < NR_BIB; ++i)
+				block[fstb.idx][way].bib[i] = cache_L2_read(blk_addr+i, 1);
+		}
+
+		return *(uint32_t*)(block[fstb.idx][way].bib + fstb.aib) & (0xFFFFFFFFu >> ((4-len)<<3));
+	}
+}
+/*
+		}
 	cache_addr temp;
 	temp.addr = addr;
 	uint32_t tag = temp.tag;
 	uint32_t index = temp.index;
 	uint32_t byte = temp.byte;
 
-	/* Judge whether the first byte is in the cache */
-	int j;
+*/	/* Judge whether the first byte is in the cache */
+/*	int j;
 	for (j = 0; j < NR_WAY; ++j)
 		if ( block[index][j].valid && (block[index][j].tag == tag) )
 			break;
@@ -90,8 +135,8 @@ uint32_t cache_read(hwaddr_t addr, size_t len)
 		for (k = 0; k < NR_BIB; ++k)
 			data[k] = block[index][j].bib[k];
 
-		/* data cross the boundary */
-		cache_addr temp2;
+*/		/* data cross the boundary */
+/*		cache_addr temp2;
 		temp2.addr = addr + len - 1;
 		uint32_t tag2 = temp2.tag;
 		uint32_t index2 = temp2.index;
@@ -157,7 +202,7 @@ uint32_t cache_read(hwaddr_t addr, size_t len)
 		return cache_L2_read(addr, len);
 	} 
 }
-
+*/
 /* Write Through and Not Write Allocate */
 void cache_write(hwaddr_t addr, size_t len, uint32_t data)
 {
@@ -166,8 +211,8 @@ void cache_write(hwaddr_t addr, size_t len, uint32_t data)
 	cache_addr temp;
 	temp.addr = addr;
 	uint32_t tag = temp.tag;
-	uint32_t index = temp.index;
-	uint32_t byte = temp.byte;
+	uint32_t index = temp.idx;
+	uint32_t byte = temp.aib;
 
 	/* Judge whether the first byte is in the cache */
 	int j;
@@ -182,7 +227,7 @@ void cache_write(hwaddr_t addr, size_t len, uint32_t data)
 		cache_addr temp2;
 		temp2.addr = addr + len - 1;
 		uint32_t tag2 = temp2.tag;
-		uint32_t index2 = temp2.index;
+		uint32_t index2 = temp2.idx;
 
 		if ( index2 == index)
 		{
