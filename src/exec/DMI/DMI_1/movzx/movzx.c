c75187f334445ddcd9787369fe287ff39638fb76
jyy <njujiangyy@gmail.com>
2014-12-11 01:33:01 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  01:33:01 up  6:17,  4 users,  load average: 0.09, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      `-2*[sshd---sshd---bash]      |-tmux-+-bash---vi      |      `-bash---make---sh-+-git      |                         `-pstree      `-udevd---2*[udevd]

--- src/exec/DMI/DMI_1/movzx/movzx.c
+++ src/exec/DMI/DMI_1/movzx/movzx.c
@@ -13,13 +13,13 @@ make_helper(movzbv)
 		if (m.mod != 3)
 		{
 			swaddr_t mem_i;  uint8_t len = read_ModR_M(eip+1, &mem_i);
-			reg_w(m.reg) = swaddr_read(mem_i, 1);
+			reg_w(m.reg) = (uint8_t)swaddr_read(mem_i, 1);
 			print_asm("movzbw  " "%s,%%%s", ModR_M_asm, regsw[m.reg]);
 			return 1 + len;
 		}
 		else
 		{
-			reg_w(m.reg) = reg_b(m.R_M);
+			reg_w(m.reg) = (uint8_t)reg_b(m.R_M);
 			print_asm("movzbw  " "%%%s,%%%s", regsb[m.R_M], regsw[m.reg]);
 			return 1 + 1;
 		}
@@ -30,13 +30,13 @@ make_helper(movzbv)
 		if (m.mod != 3)
 		{
 			swaddr_t mem_i;  uint8_t len = read_ModR_M(eip+1, &mem_i);
-			reg_l(m.reg) = swaddr_read(mem_i, 1);
+			reg_l(m.reg) = (uint8_t)swaddr_read(mem_i, 1);
 			print_asm("movzbl  " "%s,%%%s", ModR_M_asm, regsl[m.reg]);
 			return 1 + len;
 		}
 		else
 		{
-			reg_l(m.reg) = reg_b(m.R_M);
+			reg_l(m.reg) = (uint8_t)reg_b(m.R_M);
 			print_asm("movzbl  " "%%%s,%%%s", regsb[m.R_M], regsl[m.reg]);
 			return 1 + 1;
 		}
@@ -49,13 +49,13 @@ make_helper(movzwl)
 	if (m.mod != 3)
 	{
 		swaddr_t mem_i;  uint8_t len = read_ModR_M(eip+1, &mem_i);
-		reg_l(m.reg) = swaddr_read(mem_i, 2);
+		reg_l(m.reg) = (uint16_t)swaddr_read(mem_i, 2);
 		print_asm("movzwl  " "%s,%%%s", ModR_M_asm, regsl[m.reg]);
 		return 1 + len;
 	}
 	else
 	{
-		reg_l(m.reg) = reg_w(m.R_M);
+		reg_l(m.reg) = (uint16_t)reg_w(m.R_M);
 		print_asm("movzwl  " "%%%s,%%%s", regsw[m.R_M], regsl[m.reg]);
 		return 1 + 1;
 	}
