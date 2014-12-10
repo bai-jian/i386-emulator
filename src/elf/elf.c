0b7295d8cb7134b1f28e8f2b9bc349417bef466b
jyy <njujiangyy@gmail.com>
2014-12-10 10:14:40 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  10:14:40 up 1 day, 15:03,  4 users,  load average: 0.08, 0.03, 0.05 init-+-2*[tmux---9*[bash]]      |-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/elf/elf.c
+++ src/elf/elf.c
@@ -18,10 +18,10 @@ swaddr_t symbol(char* name)
 	int i;
 	for (i = 0; i < nr_symtab_entry; ++i)
 	{
-		if ( /*(symtab[i].st_info == STT_OBJECT || symtab[i].st_info == STT_FUNC)  && */\
+		if ( (symtab[i].st_info == STT_OBJECT || symtab[i].st_info == STT_FUNC)  && \
 			 (strcmp(name, strtab + symtab[i].st_name) == 0 ) )
 			return symtab[i].st_value;
-		if (i == 7)  assert(symtab[i].st_info == STT_OBJECT);
+		if (i == 7)  assert(symtab[i].st_info == STT_FUNC);
 	}
 	return 0;
 }
