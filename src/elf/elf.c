adf8953de4a3b7b07806805ca41fd8f091f8e44b
jyy <njujiangyy@gmail.com>
2014-12-10 10:58:15 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  10:58:15 up 1 day, 15:47,  4 users,  load average: 0.08, 0.04, 0.05 init-+-2*[tmux---9*[bash]]      |-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      |-sshd---sshd---bash---man---pager      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/elf/elf.c
+++ src/elf/elf.c
@@ -18,10 +18,10 @@ swaddr_t symbol(char* name)
 	int i;
 	for (i = 0; i < nr_symtab_entry; ++i)
 	{
-		if ( ( (symtab[i].st_info & 0x0F)  ==  STT_OBJECT  \
-			|| (symtab[i].st_info & 0x0F)  ==  STT_FUNC)  \
-			&& (strcmp(name, strtab + symtab[i].st_name) == 0 ) )
-			return symtab[i].st_value;
+		if (  (symtab[i].st_info & 0x0F)  ==  STT_OBJECT  || (symtab[i].st_info & 0x0F)  ==  STT_FUNC  )
+			if ( strcmp(name, strtab + symtab[i].st_name) == 0 )
+				return symtab[i].st_value;
+		Log("%d", symtab[i].st_info);
 	}
 	return 0;
 }
