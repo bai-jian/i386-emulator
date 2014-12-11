4bb347204cfa4de2fca59378644559b76ca52ee5
jyy <njujiangyy@gmail.com>
2014-12-11 09:53:09 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  09:53:09 up  7:28,  4 users,  load average: 0.14, 0.06, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---tmux]      |      `-sshd---sshd---bash      |-tmux-+-3*[bash---vi]      |      `-bash---make---sh-+-git      |                         `-pstree      `-udevd---2*[udevd]

--- myloader/src/elf.c
+++ myloader/src/elf.c
@@ -10,12 +10,12 @@ void loader( )
 	Elf32_Ehdr* elf = (void*) ELF_START;
 	Elf32_Phdr* ph = (void*)elf->e_phoff;
 
-	int i;
+	uint32_t i;
 	for (i = 0; i < elf->e_phnum; ++i)
 	{
 		if (ph[i].p_type == PT_LOAD)
 		{
-			int j;
+			uint32_t j;
 			for (j = 0; j < ph[i].p_filesz; ++j)
 				*(uint8_t*)((uint8_t*)ph[i].p_vaddr + j) = *(uint8_t*)((uint8_t*)ph[i].p_offset + j);
 			for (; j < ph[i].p_memsz; ++j)
