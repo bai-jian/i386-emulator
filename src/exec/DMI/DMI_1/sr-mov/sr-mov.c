1945a8f9d258581459ba86fdfa58aadef6003d1b
jyy <njujiangyy@gmail.com>
2014-12-13 01:05:45 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  01:05:45 up 15:28,  4 users,  load average: 0.12, 0.06, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-sshd---sshd---bash---vi      `-udevd---2*[udevd]

--- src/exec/DMI/DMI_1/sr-mov/sr-mov.c
+++ src/exec/DMI/DMI_1/sr-mov/sr-mov.c
@@ -21,9 +21,9 @@ make_helper(mov_cr2r)
 make_helper(mov_r2cr)
 {
 	ModR_M m;  m.val = instr_fetch(eip+1, 1);
-assert(0);
-	cpu.CR[m.R_M] = reg_l(m.reg);
 
+	cpu.CR[m.R_M] = reg_l(m.reg);
+assert(0);
 	print_asm("movl   %%%s, %%cr%d", regsl[m.reg], m.R_M);
 
 	return 1 + 1;
