49c0b7cf512e0e704f215db2971eefbdae90cc81
jyy <njujiangyy@gmail.com>
2014-12-12 15:47:52 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  15:47:52 up  7:10,  3 users,  load average: 0.16, 0.05, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-sshd---sshd---bash      `-udevd---2*[udevd]

--- Kernel/include/common.h
+++ Kernel/include/common.h
@@ -1,29 +1,32 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+
 /* Uncomment these macros to enable corresponding functionality. */
-//#define IA32_SEG
+#define IA32_SEG
 //#define IA32_PAGE
 //#define IA32_INTR
 //#define HAS_DEVICE
 
-#ifndef __ASSEMBLER__
-/* The following code will be included if the source file is a "*.c" file. */
 
-#include "trap.h"
+	#ifndef __ASSEMBLER__
 
-#include "stdint.h"
-#include "sys/types.h"
+		/* The following code will be included if the source file is a "*.c" file. */
 
-typedef uint8_t bool;
+		#include "trap.h"
 
-#define true 1
-#define false 0
+		#include "stdint.h"
+		#include "sys/types.h"
 
-#define NULL ((void *)0)
+		typedef uint8_t bool;
+		#define true 1
+		#define false 0
 
-#include "debug.h"
+		#define NULL ((void *)0)
 
-#endif
+		#include "debug.h"
+
+	#endif
+
 
 #endif
