b8faad109a64c65362b176d847305cf43e581461
jyy <njujiangyy@gmail.com>
2014-12-13 00:13:38 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  00:13:38 up 14:36,  4 users,  load average: 0.09, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---vi      `-udevd---2*[udevd]

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,10 +1,53 @@
 #include "common.h"
 
 
+#include "cpu/reg.h"
+
+
+lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg);
+
+uint32_t lnaddr_read(lnaddr_t addr, size_t len);
+void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data);
+
 uint32_t swaddr_read(swaddr_t addr, size_t len);
 void swaddr_write(swaddr_t addr, size_t len, uint32_t data);
 uint32_t instr_fetch(swaddr_t addr, size_t len);
 
+
+uint32_t swaddr_read(swaddr_t addr, size_t len)
+{
+	assert(len == 1 || len == 2 || len == 4);
+	if (cpu.CR0_PE == 0)
+		return lnaddr_read(addr, len);
+	else
+	{
+		lnaddr_t lnaddr = segment_translate(addr, len, current_sreg);
+		return lnaddr_read(lnaddr, len);
+	}
+}
+
+void swaddr_write(swaddr_t addr, size_t len, uint32_t data)
+{
+	assert(len == 1 || len == 2 || len == 4);
+	if (cpu.CR0_PE == 0)
+		return lnaddr_write(addr, len, data);
+	else
+	{
+		lnaddr_t lnaddr = segment_translate(addr, len, current_sreg);
+		return lnaddr_write(lnaddr, len, data);
+	}
+}
+uint32_t instr_fetch(swaddr_t addr, size_t len)
+{
+	assert(len == 1 || len == 2 || len == 4);
+	if (cpu.CR0_PE == 0)
+		return lnaddr_read(addr, len);
+	else
+	{
+		lnaddr_t lnaddr = segment_translate(addr, len, R_CS);
+		return lnaddr_read(lnaddr, len);
+	}
+}
 /*
 uint32_t cache_read(hwaddr_t addr, size_t len);
 void cache_write(hwaddr_t addr, size_t len, uint32_t data);
--- src/memory/seg_tran.c
+++ src/memory/seg_tran.c
@@ -1,33 +1,6 @@
 #include "common.h"
 
-
 #include "cpu/reg.h"
-
-uint32_t lnaddr_read(lnaddr_t addr, size_t len);
-void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data);
-
-
-lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg);
-
-uint32_t swaddr_read(swaddr_t addr, size_t len)
-{
-	assert(len == 1 || len == 2 || len == 4);
-	lnaddr_t lnaddr = segment_translate(addr, len, current_sreg);
-	return lnaddr_read(lnaddr, len);
-}
-
-void swaddr_write(swaddr_t addr, size_t len, uint32_t data)
-{
-	assert(len == 1 || len == 2 || len == 4);
-	lnaddr_t lnaddr = segment_translate(addr, len, current_sreg);
-	return lnaddr_write(lnaddr, len, data);
-}
-uint32_t instr_fetch(swaddr_t addr, size_t len)
-{
-	assert(len == 1 || len == 2 || len == 4);
-	lnaddr_t lnaddr = segment_translate(addr, len, R_CS);
-	return lnaddr_read(lnaddr, len);
-}
 
 
 lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg)
