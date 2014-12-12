774621b5490646067ee9dc850ee9aabae88a5884
jyy <njujiangyy@gmail.com>
2014-12-13 00:51:06 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  00:51:06 up 15:14,  4 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-sshd---sshd---bash---vi      `-udevd---2*[udevd]

--- src/exec/DMI/DMI_1/sr-mov/sr-mov.c
+++ src/exec/DMI/DMI_1/sr-mov/sr-mov.c
@@ -5,26 +5,26 @@
 
 
 
-// movl   %cr0/%cr1/%cr2/%cr3, r32
+// movl   r32, %cr0/%cr1/%cr2/%cr3
 make_helper(mov_cr2r)
 {
 	ModR_M m;  m.val = instr_fetch(eip+1, 1);
 	
 	reg_l(m.reg) = cpu.CR[m.R_M];
 
-	print_asm("movl   %%%s, %%cr%d", regsl[m.reg], m.R_M);
+	print_asm("movl   %%cr%d, %%%s", m.R_M, regsl[m.reg]);
 
 	return 1 + 1;
 }
 
-// movl   r32, %cr0/%cr1/%cr2/%cr3
+// movl   %cr0/%cr1/%cr2/%cr3, r32
 make_helper(mov_r2cr)
 {
 	ModR_M m;  m.val = instr_fetch(eip+1, 1);
 
 	cpu.CR[m.R_M] = reg_l(m.reg);
 
-	print_asm("movl   %%cr%d, %%%s", m.R_M, regsl[m.reg]);
+	print_asm("movl   %%%s, %%cr%d", regsl[m.reg], m.R_M);
 
 	return 1 + 1;
 }
