abf3550ee22bd9b8d925dec53c55c99293c46a5f
jyy <njujiangyy@gmail.com>
2014-12-13 13:28:13 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  13:28:13 up 19:36,  4 users,  load average: 0.09, 0.05, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---vi      `-udevd---2*[udevd]

--- src/memory/seg_tran.c
+++ src/memory/seg_tran.c
@@ -12,7 +12,6 @@ lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg)
 	// descriptor:     63...56   55  54  53  52     51...48     47  46 45  44  43...41  40   39...16     15...0
 	//               base[31-24] G   D   0   AVL  limit[19-16]  P    DPL   S    TYPE    A   base[23-0] limit[15-0] 
 
-assert(cur_sreg == R_CS);
 
 	uint8_t RPL = cpu.sreg[cur_sreg].RPL;
 	uint8_t TI = cpu.sreg[cur_sreg].TI;
@@ -33,9 +32,8 @@ assert(cur_sreg == R_CS);
 	uint8_t CPL = cpu.sreg[R_CS].RPL;
 	if ( !(DPL >= RPL && DPL >= CPL) )  assert(0);
 
-
 	uint32_t base = ( (descriptor >> 16) & 0x00FFFFFF ) + (descriptor >> 56 << 24);
 	uint32_t lnaddr = base + addr;
-
+	Log("%d %d %d\n", base, addr, lnaddr);
 	return lnaddr;
 }
