40ee27822e7c48258a99c7ad080926830865f92a
jyy <njujiangyy@gmail.com>
2014-12-13 13:02:42 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  13:02:42 up 19:11,  4 users,  load average: 0.07, 0.06, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---vi      `-udevd---2*[udevd]

--- src/cpu/cpu-exec.c
+++ src/cpu/cpu-exec.c
@@ -51,7 +51,7 @@ void cpu_exec(volatile uint32_t n)
 	for(; n > 0; --n) 
   	{
 		swaddr_t eip_temp = cpu.eip;
-if (cpu.eip == 0x1000d) assert(0);
+if (cpu.eip == 0x10000d) assert(0);
 		int instr_len = exec(cpu.eip);
  	 	if(n_temp != -1 || (enable_debug && !quiet)) 
 		{
