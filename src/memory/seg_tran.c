3cea990aefc775a9558af9d766f288c8e0a65ab4
jyy <njujiangyy@gmail.com>
2014-12-13 13:33:28 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  13:33:28 up 19:42,  4 users,  load average: 0.02, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---vi      `-udevd---2*[udevd]

--- src/memory/seg_tran.c
+++ src/memory/seg_tran.c
@@ -17,7 +17,7 @@ lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg)
 	uint8_t TI = cpu.sreg[cur_sreg].TI;
 	uint32_t index = cpu.sreg[cur_sreg].index;
 
-	Log("%d %d %d\n", addr, addr, index);
+	Log("%x %x %x\n", addr, addr, index);
 	uint64_t descriptor = *( (uint64_t*)((TI == 0) ? cpu.GDTR : cpu.LDTR) + index );
 	uint8_t DPL = (descriptor >> 45) & 0x03;
 
