3dbe4205ef80bfd837c8d9becfc2a00d5678e061
jyy <njujiangyy@gmail.com>
2014-12-14 22:38:21 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  22:38:21 up  7:31,  4 users,  load average: 0.14, 0.09, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-sshd---sshd---bash---vi      |-tmux---2*[bash]      `-udevd---2*[udevd]

--- src/memory/seg_tran.c
+++ src/memory/seg_tran.c
@@ -3,6 +3,7 @@
 #include "cpu/reg.h"
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len);
+uint32_t hwaddr_read(hwaddr_t addr, size_t len);
 
 lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg)
 {
@@ -22,13 +23,13 @@ lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg)
 	uint32_t descriptor_l, descriptor_h;
 	if (TI == 0)
 	{
-		descriptor_l = lnaddr_read(cpu.GDTR + 8 * index, 4);
-		descriptor_h = lnaddr_read(cpu.GDTR + 4 + 8 * index, 4);
+		descriptor_l = hwaddr_read(cpu.GDTR + 8 * index, 4);
+		descriptor_h = hwaddr_read(cpu.GDTR + 4 + 8 * index, 4);
 	}
 	else
 	{
-		descriptor_l = lnaddr_read(cpu.LDTR + 8 * index, 4);
-		descriptor_h = lnaddr_read(cpu.LDTR + 4 + 8 * index, 4);
+		descriptor_l = hwaddr_read(cpu.LDTR + 8 * index, 4);
+		descriptor_h = hwaddr_read(cpu.LDTR + 4 + 8 * index, 4);
 	}
 	uint8_t DPL = (descriptor_h >> 13) & 0x03;
 	
