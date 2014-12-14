255b846cbaf1f6253fe8c60e742a0592b3f63f90
jyy <njujiangyy@gmail.com>
2014-12-15 01:20:12 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  01:20:12 up 10:13,  4 users,  load average: 0.08, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      |-sshd---sshd---bash---vi      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      |-tmux---2*[bash]      `-udevd---2*[udevd]

--- src/exec/ALI/ALI_1/call/call.c
+++ src/exec/ALI/ALI_1/call/call.c
@@ -1,37 +1,57 @@
 #include "exec/helper.h"
 
+#include "cpu/reg.h"
+#include "cpu/modrm.h"
+#include "memory.h"
 
-#define DATA_BYTE 2
-#include "call-template.h"
-#undef DATA_BYTE
-
-#define DATA_BYTE 4
-#include "call-template.h"
-#undef DATA_BYTE
-
-
-extern char suffix;
-
-make_helper(call_i_v)
+make_helper(call_i)
 {
-	if (suffix == 'l')
-		return call_i_l(eip);
-	else
-	{
-		uint8_t instr_len = call_i_w(eip);
-		cpu.eip = cpu.eip & 0x0000FFFF;
-		return instr_len;
-	}
+	uint8_t instr_len = 1 + 4;
+
+	int32_t disp = instr_fetch(eip+1, 4);
+
+	// Push the return address into stack
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, eip + instr_len);
+	// Write cpu.eip
+	cpu.eip = (int32_t)cpu.eip + disp;
+
+	print_asm("call   %x", disp);
+
+	return instr_len;
 }
 
-make_helper(call_rm_v)
+make_helper(call_rm)
 {
-	if (suffix == 'l')
-		return call_rm_l(eip);
+	uint8_t instr_len = 1;
+
+	ModR_M m;	m.val = instr_fetch(eip+1, 1);
+	if (m.mod != 3)
+	{
+		swaddr_t mem_i;	 instr_len += read_ModR_M(eip+1, &mem_i);
+		uint32_t addr = swaddr_read(mem_i, 4);
+
+		// Push the return address into stack
+		cpu.esp -= 4;
+		swaddr_write(cpu.esp, 4, eip+instr_len);
+		// Write eip
+		cpu.eip = addr - instr_len;
+
+		print_asm("call   *%s", ModR_M_asm);
+
+		return instr_len;
+	}
 	else
 	{
-		uint8_t instr_len = call_rm_w(eip);
-		cpu.eip = cpu.eip & 0x0000FFFF;
+		instr_len += 1;
+		// Push the return address into stack
+		cpu.esp -= 4;
+		swaddr_write(cpu.esp, 4, eip+instr_len);
+		// Write eip
+		cpu.eip = reg_l(m.R_M) - instr_len;
+
+		print_asm("call   *%%%s", regsl[m.R_M]);
+
 		return instr_len;
 	}
 }
