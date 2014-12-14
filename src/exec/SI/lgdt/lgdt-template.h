3b7ceb35a3f378387e7c2d060f771e8b68c53c2f
jyy <njujiangyy@gmail.com>
2014-12-14 23:23:08 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  23:23:08 up  8:16,  4 users,  load average: 0.08, 0.05, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---tmux      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      |-tmux---2*[bash]      `-udevd---2*[udevd]

--- src/exec/SI/lgdt/lgdt-template.h
+++ src/exec/SI/lgdt/lgdt-template.h
@@ -6,15 +6,16 @@
 
 
 // lgdt  %GDTR, m16/m32(linear address, not virtual address)
-uint32_t swaddr_read(lnaddr_t addr, size_t len);
+uint32_t lnaddr_read(lnaddr_t addr, size_t len);
 
 make_helper( concat(lgdt_, SUFFIX) )
 {
 	ModR_M m;  m.val = instr_fetch(eip+1, 1);
 	if (m.R_M == R_EBP && m.mod == 0)
 	{
-		uint32_t lnaddr = swaddr_read(eip+2, 4);
-		cpu.GDTR = swaddr_read(lnaddr, DATA_BYTE);
+		// m16/m32 must be a displacement
+		uint32_t lnaddr = lnaddr_read(eip+2, 4);
+		cpu.GDTR = lnaddr_read(lnaddr, DATA_BYTE);
 
 		print_asm("lgdt   0x%x", lnaddr);
 
