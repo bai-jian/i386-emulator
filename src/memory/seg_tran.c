4bbfac3e48c2efbcb633083d746d84a29424fb4f
jyy <njujiangyy@gmail.com>
2014-12-14 15:30:45 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  15:30:45 up 23 min,  3 users,  load average: 0.11, 0.04, 0.02 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/memory/seg_tran.c
+++ src/memory/seg_tran.c
@@ -2,6 +2,7 @@
 
 #include "cpu/reg.h"
 
+uint32_t lnaddr_read(lnaddr_t addr, size_t len);
 
 lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg)
 {
@@ -9,21 +10,27 @@ lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg)
 	//                index    TI  RPL
 	//
 	//
-	// descriptor:     63...56   55  54  53  52     51...48     47  46 45  44  43...41  40   39...16     15...0
-	//               base[31-24] G   D   0   AVL  limit[19-16]  P    DPL   S    TYPE    A   base[23-0] limit[15-0] 
+	// descriptor:     63...56   55  54  53  52     51...48     47  46 45  44  43...41  40   39...32     31...16     15...0
+	//               base[31-24] G   D   0   AVL  limit[19-16]  P    DPL   S    TYPE    A   base[23-16] base[15-0] limit[15-0] 
 
 
-//	uint8_t RPL = cpu.sreg[cur_sreg].RPL;
+	uint8_t RPL = cpu.sreg[cur_sreg].RPL;
 	uint8_t TI = cpu.sreg[cur_sreg].TI;
 	uint32_t index = cpu.sreg[cur_sreg].index;
 
 	Log("%x %x %x\n", addr, addr, index);
-//	uint64_t descriptor;
-//	if (TI == 0)
-//		uint64_t descriptor = * ( (uint64_t*)cpu.GDTR + index );
-	uint64_t descriptor = *( (uint64_t*)((TI == 0) ? cpu.GDTR : cpu.LDTR) + index );
-	Log("desc = %x\n", (uint32_t)descriptor);
-//	uint8_t DPL = (descriptor >> 45) & 0x03;
+	uint32_t descriptor_l, descriptor_h;
+	if (TI == 0)
+	{
+		descriptor_l = lnaddr_read(cpu.GDTR + 8 * index, 4);
+		descriptor_h = lnaddr_read(cpu.GDTR + 4 + 8 * index, 4);
+	}
+	else
+	{
+		descriptor_l = lnaddr_read(cpu.LDTR + 8 * index, 4);
+		descriptor_h = lnaddr_read(cpu.LDTR + 4 + 8 * index, 4);
+	}
+	uint8_t DPL = (descriptor_h >> 13) & 0x03;
 	
 
 	/* Priviledge */
@@ -34,12 +41,12 @@ lnaddr_t segment_translate(swaddr_t addr, size_t len, uint8_t cur_sreg)
 	// An changing-over of some data segment is legal
 	//         iff    (1) target_descriptor.DPL >= requestor.RPL  &&
 	//                (2) target_descriptor.DPL >= current_program.RPL
-//	uint8_t CPL = cpu.sreg[R_CS].RPL;
-//	if ( !(DPL >= RPL && DPL >= CPL) )  assert(0);
+	uint8_t CPL = cpu.sreg[R_CS].RPL;
+	if ( !(DPL >= RPL && DPL >= CPL) )  assert(0);
 
 
-	uint32_t base = ( (descriptor >> 16) & 0x00FFFFFF ) + (descriptor >> 56 << 24);
+	uint32_t base = ((descriptor_l >> 16) & 0x0000FFFF) + ((descriptor_h << 16) & 0x00FF0000) + (descriptor_h & 0xFF000000);
 	uint32_t lnaddr = base + addr;
-
+Log("%d %d\n", base, lnaddr);
 	return lnaddr;
 }
