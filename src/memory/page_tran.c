2163cbac516ba4b2652a09a7f5bbd8c2c7a34795
jyy <njujiangyy@gmail.com>
2014-12-15 13:19:24 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  13:19:24 up 13:13,  4 users,  load average: 0.09, 0.07, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/memory/page_tran.c
+++ src/memory/page_tran.c
@@ -47,7 +47,7 @@ hwaddr_t page_translate(lnaddr_t addr)
 	page_t page_term; *(uint32_t*)(&page_term) = hwaddr_read((page_base<<12)+(lnaddr.page<<2), 4);
 	hwaddr_t base = page_term.PB;
 	hwaddr_t hwaddr = (base << 12) + lnaddr.vpo;
-	Log("%x \n", hwaddr);  assert(0);
+	Log("%x \n", hwaddr);
 
 	return hwaddr;
 }
