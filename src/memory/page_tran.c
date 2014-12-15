176d60abd7cf5e4e4bd64393561b879cc7384117
jyy <njujiangyy@gmail.com>
2014-12-15 17:03:24 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  17:03:24 up 16:57,  4 users,  load average: 0.12, 0.05, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-2*[sshd---sshd---bash]      `-udevd---2*[udevd]

--- src/memory/page_tran.c
+++ src/memory/page_tran.c
@@ -54,8 +54,12 @@ hwaddr_t page_translate(lnaddr_t addr)
 
 	hwaddr_t dir_base = cpu.CR3_pdba;
 	dir_t dir_term; *(uint32_t*)(&dir_term) = hwaddr_read((dir_base<<12)+(lnaddr.dir<<2), 4);
+	if (dir_term.P == 0)  assert(0);  // The Dir is absent.
+
 	hwaddr_t page_base = dir_term.PB;
 	page_t page_term; *(uint32_t*)(&page_term) = hwaddr_read((page_base<<12)+(lnaddr.page<<2), 4);
+	if (page_term.P == 0)  assert(0);  // The Page is absent. 
+	
 	hwaddr_t base = page_term.PB;
 	hwaddr_t hwaddr = (base << 12) + lnaddr.vpo;
 
