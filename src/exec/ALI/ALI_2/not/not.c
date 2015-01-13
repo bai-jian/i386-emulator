fea13ef47906f4515ab30b5a1cc1770043f7851f
131220095-Liu Fei <lfqhrj@qq.com>
2015-01-13 23:49:51 +0800

    new not

---
+++ src/exec/ALI/ALI_2/not/not.c
@@ -0,0 +1,20 @@
+#include "exec/helper.h"
+
+
+#define  DATA_BYTE 1
+#include "not-template.h"
+#undef   DATA_BYTE
+
+#define  DATA_BYTE 2
+#include "not-template.h"
+#undef   DATA_BYTE
+
+#define  DATA_BYTE 4
+#include "not-template.h"
+#undef   DATA_BYTE
+
+
+extern char suffix;
+
+make_helper(not_v)
+{	return  suffix == 'l'  ?  not_l(eip)  :  not_w(eip);	}
---
+++ src/exec/ALI/ALI_2/not/not.h
@@ -0,0 +1,7 @@
+#ifndef __NOT__
+#define __NOT__
+
+	make_helper(not_b);
+	make_helper(not_v);
+
+#endif
