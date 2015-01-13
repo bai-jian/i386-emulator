2a63aa12d9a0cbec59c3f65eafdd50c2c11ca76e
jyy <njujiangyy@gmail.com>
2015-01-13 21:36:07 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  21:36:07 up  3:10,  4 users,  load average: 0.18, 0.13, 0.08 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-2*[sshd---sshd---bash---tmux]      |-tmux-+-bash---vi      |      `-3*[bash]      `-udevd---2*[udevd]

--- src/exec/SI/iret/iret.c
+++ src/exec/SI/iret/iret.c
@@ -12,12 +12,8 @@ make_helper(iret)
 	cpu.eip    = (uint32_t)swaddr_read(cpu.esp, 4) - instr_len;  cpu.esp += 4;
 	cpu.CS     = (uint16_t)swaddr_read(cpu.esp, 2);              cpu.esp += 2;
 	cpu.eflags = (uint32_t)swaddr_read(cpu.esp, 4);              cpu.esp += 4;
-Log("eip 0x%x, 0x%x, 0x%x\n", cpu.eip, cpu.CS, cpu.eflags);
-	int i;
-	for (i = 0; i < 100; ++i)
-		Log("0x%x  %x", swaddr_read(cpu.esp - i, 1), swaddr_read(cpu.esp + i, 1));
-assert(0);
+
 	print_asm("iret");
-	swaddr_read(cpu.eip, 1);assert(0);
+
 	return instr_len;
 }
