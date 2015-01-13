3bef7aff38d70575c792a2b3653c5759733b1c00
jyy <njujiangyy@gmail.com>
2015-01-13 10:55:14 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  10:55:14 up 19:42,  4 users,  load average: 0.08, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      |-sshd---sshd---bash---vi      |      `-sshd---sshd---bash---tmux      |-tmux---2*[bash]      `-udevd---2*[udevd]

--- src/exec/SI/lgdt/lgdt.c
+++ src/exec/SI/lgdt/lgdt.c
@@ -10,15 +10,31 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len);
 make_helper(lgdt)
 {
 	// ModR_M:  mod  reg  R_M
-	//           00  010  101
+	//           xx  010  xxx
 	ModR_M m;  m.val = instr_fetch(eip+1, 1);
-	assert( m.val == 0x15 );
+	assert(m.reg == 2);
 
-	uint32_t ph_addr = hwaddr_read(eip+2, 4);
-	cpu.GDTR.limit = hwaddr_read(ph_addr    , 2);
-	cpu.GDTR.base  = hwaddr_read(ph_addr + 2, 4);
+	if (m.mod != 3)
+	{
+		hwaddr_t ph_addr;
+		uint8_t len = read_ModR_M(eip + 1, &ph_addr);
 
-	print_asm("lgdt   0x%x", ph_addr);
+		cpu.GDTR.limit = hwaddr_read(ph_addr    , 2);
+		cpu.GDTR.base  = hwaddr_read(ph_addr + 2, 4);
 
-	return 1 + 1 + 4;
+		print_asm("lgdt   %s", ModR_M_asm);
+
+		return 1 + len;
+	}
+	else
+	{
+		hwaddr_t ph_addr = reg_l(m.R_M);
+
+		cpu.GDTR.limit = hwaddr_read(ph_addr    , 2);
+		cpu.GDTR.base  = hwaddr_read(ph_addr + 2, 4);
+
+		print_asm("lgdt   %%%s", regsl[m.R_M]);
+
+		return 1 + 1;
+	}
 }
--- src/exec/SI/lidt/lidt.c
+++ src/exec/SI/lidt/lidt.c
@@ -10,15 +10,31 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len);
 make_helper(lidt)
 {
 	// ModR_M:  mod  reg  R_M
-	//           00  011  101
+	//           xx  011  xxx
 	ModR_M m;  m.val = instr_fetch(eip+1, 1);
-	assert( m.val == 0x1D );
+	assert(m.reg == 3);
 
-	uint32_t ph_addr = hwaddr_read(eip+2, 4);
-	cpu.IDTR.limit = hwaddr_read(ph_addr    , 2);
-	cpu.IDTR.base  = hwaddr_read(ph_addr + 2, 4);
+	if (m.mod != 3)
+	{
+		hwaddr_t ph_addr;
+		uint8_t len = read_ModR_M(eip + 1, &ph_addr);
 
-	print_asm("lidt   0x%x", ph_addr);
+		cpu.IDTR.limit = hwaddr_read(ph_addr    , 2);
+		cpu.IDTR.base  = hwaddr_read(ph_addr + 2, 4);
 
-	return 1 + 1 + 4;
+		print_asm("lidt   %s", ModR_M_asm);
+
+		return 1 + len;
+	}
+	else
+	{
+		hwaddr_t ph_addr = reg_l(m.R_M);
+
+		cpu.IDTR.limit = hwaddr_read(ph_addr    , 2);
+		cpu.IDTR.base  = hwaddr_read(ph_addr + 2, 4);
+		
+		print_asm("lidt   %%%s", regsl[m.R_M]);
+
+		return 1 + 1;
+	}
 }
