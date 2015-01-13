2889205e0d8381fb8583422849efa4ef0b08695c
jyy <njujiangyy@gmail.com>
2015-01-13 20:40:36 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  20:40:36 up  2:14,  4 users,  load average: 0.18, 0.09, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-2*[sshd---sshd---bash---tmux]      |-tmux-+-bash---vi      |      `-3*[bash]      `-udevd---2*[udevd]

--- src/exec/SI/iret/iret.c
+++ src/exec/SI/iret/iret.c
@@ -15,7 +15,7 @@ make_helper(iret)
 
 	int i;
 	for (i = 0; i < 100; ++i)
-		Log("0x%.2x  %.2x", cpu.esp - 2 * i, cpu.esp + 2 * i);
+		Log("0x%x  %x", cpu.esp - i, cpu.esp + i);
 assert(0);
 	print_asm("iret");
 	swaddr_read(cpu.eip, 1);assert(0);
