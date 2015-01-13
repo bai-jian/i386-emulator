204e810bc17123f0e2c6cc19e600e93c1439cfa1
jyy <njujiangyy@gmail.com>
2015-01-13 21:25:35 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  21:25:35 up  2:59,  4 users,  load average: 0.16, 0.07, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-2*[sshd---sshd---bash---tmux]      |-tmux-+-2*[bash]      |      `-2*[bash---vi]      `-udevd---2*[udevd]

--- src/exec/DMI/DMI_3/pop/pop-template.h
+++ src/exec/DMI/DMI_3/pop/pop-template.h
@@ -10,19 +10,22 @@ make_helper( concat(pop_r_, SUFFIX) )
 	REG(reg_i) = MEM_R(cpu.esp);
 	cpu.esp += DATA_BYTE;
 
-	print_asm("pop" str(SUFFIX) "  %%%s", REG_NAME(reg_i));
+	print_asm("pop" str(SUFFIX) "   %%%s", REG_NAME(reg_i));
 
 	return 1;
 }
 
 make_helper( concat(pop_m_, SUFFIX) )
 {
+	ModR_M m;  m.val = instr_fetch(eip + 1, 1);
+	assert(m.mod != 3);
+
 	swaddr_t mem_i;  uint8_t len = read_ModR_M(eip+1, &mem_i);
 
 	MEM_W(mem_i, MEM_R(cpu.esp));
 	cpu.esp += DATA_BYTE;
 
-	print_asm("pop" str(SUFFIX) "  %s", ModR_M_asm);
+	print_asm("pop" str(SUFFIX) "   %s", ModR_M_asm);
 
 	return 1 + len;
 }
