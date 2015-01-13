92b7b81e64c982831bb6cfa066ce5fc21228800b
jyy <njujiangyy@gmail.com>
2015-01-14 00:01:08 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  00:01:08 up  5:35,  4 users,  load average: 0.14, 0.06, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---tmux]      |      `-sshd---sshd---bash      |-tmux-+-2*[bash---vi]      |      |-bash      |      `-bash---make---sh-+-git      |                         `-pstree      `-udevd---2*[udevd]

--- src/exec/ALI/ALI_2/opcode/opcode.c
+++ src/exec/ALI/ALI_2/opcode/opcode.c
@@ -1,27 +1,34 @@
 #include "exec/helper.h"
 
+
 #include "cpu/modrm.h"
 
-int test_i2rm_b(swaddr_t eip);
-int imul_b(swaddr_t eip);
-int idiv_b(swaddr_t eip);
+/* ALI_2_b: F6 */
+int  test_i2rm_b (swaddr_t eip);
+int  not_b       (swaddr_t eip);
+int  imul_b      (swaddr_t eip);
+int  idiv_b      (swaddr_t eip);
 make_helper(ALI_2_b)
 {
 	ModR_M m;  m.val = instr_fetch(eip+1, 1);
-	if ( m.reg == 0 )  return test_i2rm_b(eip);
-	if ( m.reg == 5 )  return imul_b(eip);
-	if ( m.reg == 7 )  return idiv_b(eip);
+	if ( m.reg == 0 )  return  test_i2rm_b (eip);
+	if ( m.reg == 2 )  return  not_b       (eip);
+	if ( m.reg == 5 )  return  imul_b      (eip);
+	if ( m.reg == 7 )  return  idiv_b      (eip);
 	assert(0);  return 0;
 }
 
-int test_i2rm_v(swaddr_t eip);
-int imul_v(swaddr_t eip);
-int idiv_v(swaddr_t eip);
+/* ALI_2_v: F7 */
+int  test_i2rm_v (swaddr_t eip);
+int  not_v       (swaddr_t eip);
+int  imul_v      (swaddr_t eip);
+int  idiv_v      (swaddr_t eip);
 make_helper(ALI_2_v)
 {
 	ModR_M m;  m.val = instr_fetch(eip+1, 1);
-	if ( m.reg == 0 )  return test_i2rm_v(eip);
-	if ( m.reg == 5 )  return imul_v(eip);
-	if ( m.reg == 7 )  return idiv_v(eip);
+	if ( m.reg == 0 )  return  test_i2rm_v (eip);
+	if ( m.reg == 2 )  return  not_v       (eip);
+	if ( m.reg == 5 )  return  imul_v      (eip);
+	if ( m.reg == 7 )  return  idiv_v      (eip);
 	assert(0);  return 0;
 }
