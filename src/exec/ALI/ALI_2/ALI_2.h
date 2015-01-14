478dd053111efaef97ac137a10a823f6fc602095
jyy <njujiangyy@gmail.com>
2015-01-14 10:31:09 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  10:31:09 up  9:15,  4 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---tmux]      |      `-sshd---sshd---bash      |-tmux-+-bash---make---sh-+-git      |      |                  `-pstree      |      |-2*[bash---vi]      |      `-2*[bash]      `-udevd---2*[udevd]

--- src/exec/ALI/ALI_2/ALI_2.h
+++ src/exec/ALI/ALI_2/ALI_2.h
@@ -1,4 +1,6 @@
 #include "imul/imul.h"
 #include "test/test.h"
+#include "neg/neg.h"
+#include "not/not.h"
 
 #include "opcode/opcode.h"
---
+++ src/exec/ALI/ALI_2/neg/neg.h
@@ -0,0 +1,7 @@
+#ifndef __NEG__
+#define __NEG__
+
+	make_helper(neg_b);
+	make_helper(neg_v);
+
+#endif
