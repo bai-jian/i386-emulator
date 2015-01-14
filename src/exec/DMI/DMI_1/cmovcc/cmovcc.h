4157628475f38da2c735b38491bf5e3bb61565d2
jyy <njujiangyy@gmail.com>
2015-01-14 22:32:43 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  22:32:43 up 20:28,  4 users,  load average: 0.24, 0.07, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      `-2*[sshd---sshd---bash]      `-udevd---2*[udevd]

--- src/exec/DMI/DMI_1/cmovcc/cmovcc.h
+++ src/exec/DMI/DMI_1/cmovcc/cmovcc.h
@@ -2,8 +2,8 @@
 #define __CMOVCC__
 
 /* 0F 44 */  make_helper(cmove_v);
-/* 0F 47 */  make_helper(cmova_v);
-/* 0F 48 */  make_helper(cmovs_v);
+/* 0F 47 */  make_helper(cmova_v);		// make_helper(cmovnbe_v);
+/* 0F 48 */  make_helper(cmovs_v);		
 /* 0F 4D */  make_helper(cmovge_v);		// make_helper(cmovnl_v);
 
 #endif
