3e4902ccb2e2220d5fdf327b13d1787720146957
jyy <njujiangyy@gmail.com>
2015-01-18 16:06:39 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  16:06:39 up 22 min,  3 users,  load average: 0.00, 0.12, 0.21 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash-+-make---sh-+-git      |      |                    |           `-pstree      |      |                    `-vim      |      `-sshd---sshd---bash      `-udevd---2*[udevd]

--- src/exec/SI/hlt/hlt.c
+++ src/exec/SI/hlt/hlt.c
@@ -1,7 +1,12 @@
 #include "exec/helper.h"
+#include "cpu/reg.h"
+#include "common.h"
 
 make_helper(hlt)
 {
 	//while(!(cpu.IF && cpu.INTR)) ;
+	volatile bool *p = &cpu.INTR;
+	if (cpu.IF)
+		while(*p == 0);
 	return 1;
 }
