9d9fa1c529dc38945996bf76baa7eba826555fdf
jyy <njujiangyy@gmail.com>
2015-01-19 17:44:10 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  17:44:10 up  2:17,  5 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-pstree      |      |-sshd---sshd---bash---tmux      |      `-sshd---sshd---bash---man---pager      |-tmux-+-bash      |      `-bash---vi      `-udevd---2*[udevd]

--- Kernel/src/fs/fs.c
+++ Kernel/src/fs/fs.c
@@ -45,18 +45,16 @@ void ide_write(uint8_t *, uint32_t, uint32_t);
 
 int fs_open(const char* pathname, int flags)
 {
-	Log("%s\n", pathname);
 	uint32_t i;
 	for (i = 0; i < NR_FILES; ++ i)
 		if (strcmp(pathname, file_table[i].name) == 0)
 			break;
 	if (i == NR_FILES)  assert(0);
-	Log("%s\n", file_table[i].name);
 
 	fstat[i + 3].opened = true;
 	fstat[i + 3].offset = 0;
 
-	return file_table[i].disk_offset;
+	return i + 3;
 }
 
 
